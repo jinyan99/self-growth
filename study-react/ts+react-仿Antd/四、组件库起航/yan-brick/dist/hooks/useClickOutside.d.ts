5fefd5f48d69f6627fbf588402ae3f28a4cccff7
jinyan020 <jinyan020@ke.com>
2020-09-25 23:25:12 +0800

    yan-brick transfer

--- study-react/ts+react-仿Antd/四、组件库起航/yan-brick/dist/hooks/useClickOutside.d.ts
+++
@@ -1,3 +0,0 @@
-import { RefObject } from "react";
-declare function useClickOutside(ref: RefObject<HTMLElement>, handler: (e: Event) => void): void;
-export default useClickOutside;
--- study-react/ts+react-仿Antd/四、组件库起航/yan-brick/dist/hooks/useDebounce.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @param {any} value
- * @param {number} delay 多少秒后执行
- */
-declare function useDebounce(value: any, delay?: number): any;
-export default useDebounce;
